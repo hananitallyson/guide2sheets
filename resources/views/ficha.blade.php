843df532f19d0ac9bcf93bb36e22e1dc5601ed65
hananitallyson <tallysonhanani@gmail.com>
2023-10-25 22:47:32 -0300

    Criando a página de Gerar Ficha em HTML e CSS

--- resources/views/ficha.blade.php
+++ resources/views/ficha.blade.php
@@ -1,17 +1,86 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="h-screen font-sans text-gray-900 antialiased overflow-hidden">
+        <main class="mt-6 flex flex-col justify-center items-center h-3/4">
+            <div class="py-12 w-96">
+                <div class="mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-gray-100 overflow-hidden shadow sm:rounded-lg">
+                        <form method="POST" class="p-8 flex flex-col">
+                            @csrf
+                            <div class="flex justify-between gap-2">
+                                <div class="mb-4">
+                                    <label for="raca" class="font-semibold">Raça:</label>
+                                    <select id="raca" name="raca" class="w-full border border-gray-300 p-2 rounded">
+                                        <option value="humano">Humano</option>
+                                        <option value="elfo">Elfo</option>
+                                        <option value="anão">Anão</option>
+                                        <option value="halfling">Halfling</option>
+                                        <!-- Adicione mais opções de raça aqui -->
+                                    </select>
+                                </div>
+
+                                <div class="mb-4">
+                                    <label for="classe" class="font-semibold">Classe:</label>
+                                    <select id="classe" name="classe" class="w-full border border-gray-300 p-2 rounded">
+                                        <option value="guerreiro">Guerreiro</option>
+                                        <option value="mago">Mago</option>
+                                        <option value="clérigo">Clérigo</option>
+                                        <option value="ladino">Ladino</option>
+                                        <!-- Adicione mais opções de classe aqui -->
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="flex justify-between gap-2">
+                                <div class="mb-4">
+                                    <label for="antecedente" class="font-semibold">Antecedente:</label>
+                                    <select id="antecedente" name="antecedente" class="w-full border border-gray-300 p-2 rounded">
+                                        <option value="nobre">Nobre</option>
+                                        <option value="forasteiro">Forasteiro</option>
+                                        <option value="artesão">Artesão</option>
+                                        <option value="acólito">Acólito</option>
+                                        <!-- Adicione mais opções de antecedente aqui -->
+                                    </select>
+                                </div>
+
+                                <div class="mb-4">
+                                    <label for="nivel" class="font-semibold">Nível:</label>
+                                    <select id="nivel" name="nivel" class="w-full border border-gray-300 p-2 rounded">
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <!-- Adicione mais opções de nível aqui -->
+                                    </select>
+                                </div>
+                            </div>
+
+                            <input class="bg-red-600 mt-2 py-2 px-6 text-xl rounded-full text-white font-semibold cursor-pointer" type="submit" value="Gerar Ficha!">
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</x-app-layout>
+        </main>
+
+        <footer class="bg-gray-100 w-full shadow fixed bottom-0 flex">
+            <div class="mx-auto max-w-screen-xl p-4">
+                <span class="text-sm text-gray-500 text-center">© {{ now()->year }} <a href="#" class="hover:underline">Guide2Sheets</a>. Todos os direitos reservados.
+                </span>
+            </div>
+        </footer>
+    </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,30 +15,24 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="h-screen font-sans text-gray-900 antialiased overflow-hidden">
-        <header class="bg-red-600 shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 text-white font-semibold">
-                LOGO
-            </div>
-        </header>
-
-        <main class="mt-6 flex flex-col justify-center items-center h-4/5">
+        <main class="mt-6 flex flex-col justify-center items-center h-3/4">
             <div class="py-12">
                 <div class="max-w-xl mx-auto sm:px-6 lg:px-8">
-                    <div class="bg-white overflow-hidden shadow sm:rounded-lg">
+                    <div class="bg-gray-100 overflow-hidden shadow sm:rounded-lg">
                         <div class="text-xl p-6 text-gray-900 text-center">
                             Nossa plataforma fornece uma ferramenta ideal para facilitar a criação de fichas de personagens no sistema de D&D 5E!
                         </div>
                     </div>
                 </div>
             </div>
-            <a href="#" class="bg-red-600 py-2 px-6 text-xl rounded-full text-white font-semibold">
+            <a href="/ficha" class="bg-red-600 py-2 px-6 text-xl rounded-full text-white font-semibold">
                 Criar ficha!
             </a>
         </main>
 
-        <footer class="bg-white rounded-lg shadow m-4 h-1/5">
-            <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span class="text-sm text-gray-500 sm:text-center">© {{ now()->year }} <a href="#" class="hover:underline">Guide2Sheets</a>. Todos os direitos reservados.
+        <footer class="bg-gray-100 w-full shadow fixed bottom-0 flex">
+            <div class="mx-auto max-w-screen-xl p-4">
+                <span class="text-sm text-gray-500 text-center">© {{ now()->year }} <a href="#" class="hover:underline">Guide2Sheets</a>. Todos os direitos reservados.
                 </span>
             </div>
         </footer>
